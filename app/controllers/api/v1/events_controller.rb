e51b5218acf19aad4ad51af942c3e4a9e72d2636
Luis Grases <luisgrases@gmail.com>
2015-12-24 12:48:46 +0100

    Respond with current user events

--- app/controllers/api/v1/events_controller.rb
+++ app/controllers/api/v1/events_controller.rb
@@ -3,7 +3,8 @@ module Api
     class EventsController < ApplicationController
       respond_to :json
       def index
-        respond_with current_user
+        user = User.find(current_user.id)
+        respond_with user.events
       end
 
     end
