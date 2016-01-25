99932f068f9e2def55888785062706246c10ceec
Luis Grases <luisgrases@gmail.com>
2016-01-24 23:27:19 -0600

    Query function to get invited people with status pending

--- app/models/event.rb
+++ app/models/event.rb
@@ -6,7 +6,7 @@ class Event < ActiveRecord::Base
   accepts_nested_attributes_for :users, :members
 
   def attending_friends
-    User.includes(:members).where(members: {event_id: 51})
+    User.includes(:members).where(members: {event: self, status: 'pending'})
   end
 
 end
